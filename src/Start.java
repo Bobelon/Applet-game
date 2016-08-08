0dac0570bce37613066bc714880701737f8fd801
Filip <super.dewolaj@gmail.com>
2016-08-08 18:31:57 +0200

    Pozbycie się migotania tekstu podczas poruszania się

--- src/Start.java
+++ src/Start.java
@@ -1,17 +1,29 @@
 import java.applet.Applet;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.util.Timer;
 
 public class Start extends Applet {
 
 	static Applet applet;
+	int wielkoscX = 800, wielkoscY = 600;
 	Obliczenia zad = new Obliczenia();
 	Timer timer = new Timer();
+	Image bgImage;
+	Graphics bgGrap;
 	
 	public void init() {
 		applet = this;
-		applet.setSize(800, 600);
+		applet.setSize(wielkoscX, wielkoscY);
 		timer.scheduleAtFixedRate(zad, 10, 10);
+		bgImage = createImage(wielkoscX, wielkoscY);
+		bgGrap = bgImage.getGraphics();
+	}
+	
+	public void update(Graphics g) {
+		bgGrap.clearRect(0, 0, wielkoscX, wielkoscY);
+		paint(bgGrap);
+		g.drawImage(bgImage, 0, 0, applet);
 	}
 	
 	public void paint(Graphics g){
